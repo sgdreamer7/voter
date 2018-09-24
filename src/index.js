6268383794d7ce1bb736954960e659865cf03ca3
Karine91 <kitana.91@mail.ru>
2018-09-24 19:01:57 +0300

    delete extra files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch } from "react-router-dom";
+import { Provider } from "react-redux";
+import configureStore from "./store/configureStore";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import "assets/css/material-dashboard-react.css?v=1.5.0";
+import "assets/css/styles.css";
+
+import indexRoutes from "routes/index.jsx";
+
+const hist = createBrowserHistory();
+
+const store = configureStore();
+
+const app = (
+  <Provider store={store}>
+    <Router history={hist}>
+      <Switch>
+        {indexRoutes.map((prop, key) => {
+          return (
+            <Route path={prop.path} component={prop.component} key={key} />
+          );
+        })}
+      </Switch>
+    </Router>
+  </Provider>
+);
+
+ReactDOM.render(app, document.getElementById("root"));
